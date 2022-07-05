1d6e51d08a2f792dce1fc9dfd1d3af716ac1a3b9
Sosuke Suzuki <aosukeke@gmail.com>
2022-07-06 00:47:45 +0900

    Type errors

--- src/utils/parse-markdown.ts
+++ src/utils/parse-markdown.ts
@@ -1,7 +1,12 @@
 import { remarkGfm, remarkParse, unified } from "../../deps.ts";
 
 export default function parseMarkdown(text: string) {
-  const processor = unified().use(remarkParse).use(remarkGfm);
+  const processor = unified()
+    .use(
+      // @ts-expect-error I don't know how to fix this type error
+      remarkParse
+    )
+    .use(remarkGfm);
   const res = processor.parse(text);
   return res;
 }
